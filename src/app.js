f8a9df2994431a1fc0f5cb03f899ffa838212bc5
Guillermo <guillermo.gil.cor@gmail.com>
2024-04-26 20:17:27 +0100

    generador de dominios

--- src/app.js
+++ src/app.js
@@ -1,4 +1,37 @@
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our", "mine", "yours"];
+let adj = ["great", "big", "happy", "brave", "old"];
+let noun = ["jogger", "racoon", "pack", "mountain", "river"];
+
+let newArr = [];
+
+// ejemplo1
+// for(let i =0;i<pronoun.length;i++){
+//   for(let n=0;n<adj.length;n++){
+//     for(let x=0;x<noun.length;x++){
+//       newArr.push(pronoun[i]+adj[n]+noun[x]+".com")
+//     }
+//   }
+// }
+// console.log(newArr)
+
+// ejemplo2
+// pronoun.forEach((element) =>
+//   adj.forEach((element1) =>
+//     noun.forEach((element2) =>
+//       newArr.push(element + element1 + element2 + ".com")
+//     )
+//   )
+// );
+// console.log(newArr);
+
+//ejemplo3
+newArr = pronoun
+  .map((element) =>
+    adj
+      .map((element1) =>
+        noun.map((element2) => element + element1 + element2 + ".com")
+      )
+      .flat()
+  )
+  .flat();
+console.log(newArr);
